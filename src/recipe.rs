f58e61647eac2c96fe8aa5a4051b1eddec2751d3
heptaliane <heptaliane@gmail.com>
2023-12-28 16:36:42 +0900

    Create recipe builder

--- src/recipe.rs
+++ src/recipe.rs
@@ -1,13 +1,8 @@
 use super::data::{Monster, Parent};
-use std::collections::HashMap;
-
-#[derive(Eq, PartialEq, Debug)]
-pub enum Limitation {
-    Rank([Option<usize>; 2]),
-    Scoutable(bool),
-    Standard(bool),
-    HideScouted(bool),
-}
+use std::cell::RefCell;
+use std::collections::{HashMap, HashSet};
+use std::iter::Iterator;
+use std::rc::Rc;
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct MonsterSpec {
@@ -23,47 +18,113 @@ pub struct MonsterInfo {
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct MonsterNode {
-    monster_id: usize,
-    children: Vec<MonsterNode>,
+    data: MonsterInfo,
+    children: Vec<Rc<RefCell<MonsterNode>>>,
 }
 
-fn get_parents_info(monster: &Monster) -> Vec<Vec<MonsterInfo>> {
-    monster
-        .parents
-        .iter()
-        .map(|parents| {
+struct MonsterTreeBuilder {
+    lut: HashMap<usize, Monster>,
+    prefer_standard: bool,
+}
+
+impl MonsterTreeBuilder {
+    fn new(monster_lut: HashMap<usize, Monster>) -> Self {
+        Self {
+            lut: monster_lut,
+            prefer_standard: false,
+        }
+    }
+
+    fn prefer_standard_fusion(&mut self, prefer_standard: bool) {
+        self.prefer_standard = prefer_standard;
+    }
+
+    fn get_parents_info(&self, monster_id: usize) -> Vec<Vec<MonsterInfo>> {
+        let monster = &self.lut[&monster_id];
+        monster
+            .parents
+            .iter()
+            .map(|ps| {
+                ps.iter()
+                    .map(|p| MonsterInfo {
+                        spec: match p.family {
+                            Some(id) => Some(MonsterSpec {
+                                family: id,
+                                rank: monster.rank,
+                            }),
+                            None => None,
+                        },
+                        monster_id: p.monster,
+                    })
+                    .collect()
+            })
+            .collect()
+    }
+
+    fn select_parents(&self, parents_list: Vec<Vec<MonsterInfo>>) -> Option<Vec<MonsterInfo>> {
+        let count_standard_parents = |parents: &Vec<MonsterInfo>| {
             parents
                 .iter()
-                .map(|parent| MonsterInfo {
-                    spec: match parent.family {
-                        Some(id) => Some(MonsterSpec {
-                            family: id,
-                            rank: monster.rank,
-                        }),
-                        None => None,
-                    },
-                    monster_id: parent.monster,
+                .filter(|info| info.spec.is_some())
+                .collect::<Vec<&MonsterInfo>>()
+                .len()
+        };
+
+        match self.prefer_standard {
+            true => parents_list.into_iter().max_by_key(count_standard_parents),
+            false => parents_list.into_iter().min_by_key(count_standard_parents),
+        }
+    }
+
+    fn get_child_nodes(&self, monster_id: usize) -> Vec<Rc<RefCell<MonsterNode>>> {
+        let parents_list = self.get_parents_info(monster_id);
+        let parents = self.select_parents(parents_list);
+        match parents {
+            Some(ps) => ps
+                .iter()
+                .map(|data| {
+                    Rc::new(RefCell::new(MonsterNode {
+                        data: data.clone(),
+                        children: vec![],
+                    }))
                 })
-                .collect()
-        })
-        .collect()
-}
+                .collect(),
+            None => vec![],
+        }
+    }
 
-fn select_parents(
-    parents_list: &Vec<Vec<MonsterInfo>>,
-    prefer_standard: bool,
-) -> Option<&Vec<MonsterInfo>> {
-    let count_standard_parents = |parents: &&Vec<MonsterInfo>| {
-        parents
-            .iter()
-            .filter(|info| info.spec.is_some())
-            .collect::<Vec<&MonsterInfo>>()
-            .len()
-    };
+    fn is_leaf_node(&self, node: &Rc<RefCell<MonsterNode>>) -> bool {
+        match node.borrow().data.monster_id {
+            Some(monster_id) => {
+                let monster = &self.lut[&monster_id];
+                monster.parents.len() == 0
+            }
+            None => true,
+        }
+    }
 
-    match prefer_standard {
-        true => parents_list.iter().max_by_key(count_standard_parents),
-        false => parents_list.iter().min_by_key(count_standard_parents),
+    pub fn build(&self, monster_id: usize) -> MonsterNode {
+        let root = Rc::new(RefCell::new(MonsterNode {
+            data: MonsterInfo {
+                monster_id: Some(monster_id),
+                spec: None,
+            },
+            children: vec![],
+        }));
+
+        let mut stack = vec![root.clone()];
+        while stack.len() > 0 {
+            let cursor = std::mem::replace(&mut stack, Vec::new());
+            for node in cursor.into_iter() {
+                if self.is_leaf_node(&node) {
+                    let monster_id = node.borrow().data.monster_id.unwrap();
+                    node.try_borrow_mut().unwrap().children = self.get_child_nodes(monster_id);
+                    stack.extend(node.borrow().children.iter().map(|n| n.clone()));
+                }
+            }
+        }
+
+        Rc::try_unwrap(root).unwrap().into_inner()
     }
 }
 
@@ -79,56 +140,59 @@ fn is_scoutable(monster: &Monster) -> bool {
     monster.habitats.len() > 0
 }
 
-// pub fn create_recipe_tree(
-//     monster_id: usize,
-//     monster_lut: &HashMap<usize, Monster>,
-//     priority: Vec<Limitation>,
-// ) -> MonsterNode {
-// }
-
 #[test]
 fn test_get_parents_info() {
     use super::data::Parent;
 
-    let monster1 = Monster {
-        name: "a".to_string(),
-        rank: 0,
-        family: 0,
-        parents: vec![],
-        habitats: HashMap::new(),
-    };
-    let actual1 = get_parents_info(&monster1);
+    let lut: HashMap<usize, Monster> = HashMap::from([
+        (
+            0,
+            Monster {
+                name: "a".to_string(),
+                rank: 0,
+                family: 0,
+                parents: vec![],
+                habitats: HashMap::new(),
+            },
+        ),
+        (
+            1,
+            Monster {
+                name: "a".to_string(),
+                rank: 0,
+                family: 1,
+                parents: vec![
+                    vec![
+                        Parent {
+                            monster: None,
+                            family: Some(2),
+                        },
+                        Parent {
+                            monster: None,
+                            family: Some(3),
+                        },
+                    ],
+                    vec![
+                        Parent {
+                            monster: Some(4),
+                            family: None,
+                        },
+                        Parent {
+                            monster: Some(5),
+                            family: None,
+                        },
+                    ],
+                ],
+                habitats: HashMap::new(),
+            },
+        ),
+    ]);
+
+    let builder = MonsterTreeBuilder::new(lut);
+    let actual1 = builder.get_parents_info(0);
     assert_eq!(actual1.len(), 0);
 
-    let monster2 = Monster {
-        name: "a".to_string(),
-        rank: 0,
-        family: 1,
-        parents: vec![
-            vec![
-                Parent {
-                    monster: None,
-                    family: Some(2),
-                },
-                Parent {
-                    monster: None,
-                    family: Some(3),
-                },
-            ],
-            vec![
-                Parent {
-                    monster: Some(4),
-                    family: None,
-                },
-                Parent {
-                    monster: Some(5),
-                    family: None,
-                },
-            ],
-        ],
-        habitats: HashMap::new(),
-    };
-    let actual2 = get_parents_info(&monster2);
+    let actual2 = builder.get_parents_info(1);
     assert_eq!(actual2.len(), 2);
     assert_eq!(actual2[0].len(), 2);
     assert_eq!(
@@ -149,11 +213,7 @@ fn test_get_parents_info() {
 
 #[test]
 fn test_select_parents() {
-    let parents1: Vec<Vec<MonsterInfo>> = vec![];
-    let actual1t = select_parents(&parents1, true);
-    assert_eq!(actual1t, None);
-    let actual1f = select_parents(&parents1, false);
-    assert_eq!(actual1f, None);
+    let mut builder = MonsterTreeBuilder::new(HashMap::new());
 
     let info1 = vec![
         MonsterInfo {
@@ -185,13 +245,22 @@ fn test_select_parents() {
             monster_id: Some(4),
         },
     ];
+
+    let parents1: Vec<Vec<MonsterInfo>> = vec![];
     let parents2 = vec![info1.clone(), info2.clone(), info3.clone()];
-    let actual2t = select_parents(&parents2, true);
-    assert!(actual2t.is_some());
-    assert_eq!(actual2t.unwrap().clone(), info1.clone());
-    let actual2f = select_parents(&parents2, false);
+
+    let actual1f = builder.select_parents(parents1.clone());
+    assert_eq!(actual1f, None);
+    let actual2f = builder.select_parents(parents2.clone());
     assert!(actual2f.is_some());
     assert_eq!(actual2f.unwrap().clone(), info3.clone());
+
+    builder.prefer_standard_fusion(true);
+    let actual1t = builder.select_parents(parents1.clone());
+    assert_eq!(actual1t, None);
+    let actual2t = builder.select_parents(parents2.clone());
+    assert!(actual2t.is_some());
+    assert_eq!(actual2t.unwrap().clone(), info1.clone());
 }
 
 #[test]
